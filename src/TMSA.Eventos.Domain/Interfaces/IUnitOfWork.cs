02706efd266d8970d991e9bdc3b12512310625b6
Thiago Moreira de Souza Arrais <thiagomds.scientist@gmail.com>
2018-08-11 20:41:57 -0300

    Foi criado o UnitOfWork e a interface dela também.

---
+++ src/TMSA.Eventos.Domain/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace TMSA.Eventos.Domain.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        bool Commit();
+    }
+}
---
+++ src/TMSA.Eventos.Infra.Data/UoW/UnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿using TMSA.Eventos.Domain.Interfaces;
+using TMSA.Eventos.Infra.Data.Context;
+
+namespace TMSA.Eventos.Infra.Data.UoW
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly EventoContext _context;
+
+        public UnitOfWork(EventoContext contexto)
+        {
+            _context = contexto;
+        }
+
+        public bool Commit()
+        {
+            return _context.SaveChanges() > 0;
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
