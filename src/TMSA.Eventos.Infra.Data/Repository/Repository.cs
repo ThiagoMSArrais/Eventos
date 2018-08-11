905b7849f2a44ce7fb2b876207f6f0b1be9ee549
Thiago Moreira de Souza Arrais <thiagomds.scientist@gmail.com>
2018-08-11 20:31:40 -0300

    Foi criado o repository genêrico.

---
+++ src/TMSA.Eventos.Infra.Data/Repository/Repository.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using TMSA.Eventos.Domain.Core.Models;
+using TMSA.Eventos.Domain.Interfaces;
+using TMSA.Eventos.Infra.Data.Context;
+
+namespace TMSA.Eventos.Infra.Data.Repository
+{
+    public abstract class Repository<TEntity> : IRepository<TEntity> where TEntity : Entity<TEntity>
+    {
+
+        protected EventoContext Db;
+        protected DbSet<TEntity> DbSet;
+
+        public Repository(EventoContext context)
+        {
+            Db = context;
+            DbSet = Db.Set<TEntity>();
+        }
+
+        public void Adicionar(TEntity obj)
+        {
+            DbSet.Add(obj);
+        }
+
+        public void Atualizar(TEntity obj)
+        {
+            DbSet.Update(obj);
+        }
+
+        public IEnumerable<TEntity> Buscar(Expression<Func<TEntity, bool>> predicate)
+        {
+            return DbSet.AsNoTracking().Where(predicate);
+        }
+
+        public TEntity ObterPorId(Guid id)
+        {
+            return DbSet.AsNoTracking().FirstOrDefault(t => t.Id == id);
+        }
+
+        public IEnumerable<TEntity> ObterTodos()
+        {
+            return DbSet.ToList();
+        }
+
+        public void Remover(Guid id)
+        {
+            DbSet.Remove(DbSet.Find(id));
+        }
+
+        public int SaveChanges()
+        {
+            return Db.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            Db.Dispose();
+        }
+    }
+}
